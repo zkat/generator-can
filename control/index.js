020acd9decdec8c6947cc91a5f6a8c7767aa4927
Josh Marchán <sykopomp@sykosomatic.org>
2013-12-05 15:38:12 -0600

    can:control scaffold

---
+++ control/index.js
@@ -0,0 +1,21 @@
+'use strict';
+var util = require('util');
+var yeoman = require('yeoman-generator');
+
+var ControlGenerator = module.exports = function ControlGenerator(args, options, config) {
+  // By calling `NamedBase` here, we get the argument to the subgenerator call
+  // as `this.name`.
+  yeoman.generators.NamedBase.apply(this, arguments);
+  this.destinationRoot("src/controls/"+this.name);
+  this.mkdir(this.destinationRoot());
+};
+
+util.inherits(ControlGenerator, yeoman.generators.NamedBase);
+
+ControlGenerator.prototype.files = function files() {
+  this.template("_control.js", this.name+".js");
+  this.template("test.js", "test.js");
+  this.template("test.mustache", "test.mustache");
+  this.template("styles.less", "styles.less");
+  this.template("template.mustache", "template.mustache");
+};
