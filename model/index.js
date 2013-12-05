bd4ef2d401f2434495d4a0c4eb6ae9e2dde9f981
Josh Marchán <sykopomp@sykosomatic.org>
2013-12-05 15:42:09 -0600

    can:model scaffold

---
+++ model/index.js
@@ -0,0 +1,18 @@
+'use strict';
+var util = require('util');
+var yeoman = require('yeoman-generator');
+
+var ModelGenerator = module.exports = function ModelGenerator(args, options, config) {
+  // By calling `NamedBase` here, we get the argument to the subgenerator call
+  // as `this.name`.
+  yeoman.generators.NamedBase.apply(this, arguments);
+  this.destinationRoot("src/models/"+this.name);
+  this.mkdir(this.destinationRoot());
+};
+
+util.inherits(ModelGenerator, yeoman.generators.NamedBase);
+
+ModelGenerator.prototype.files = function files() {
+  this.template("_model.js", this.name+".js");
+  this.template("test.js", "test.js");
+};
