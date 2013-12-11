a77e4114dc7e98dd275784cd4182d6fdd7cb3892
Kat Marchán <kzm@sykosomatic.org>
2013-12-11 14:06:24 -0800

    Tabify generator code

--- test/test-creation.js
+++ test/test-creation.js
@@ -1,38 +1,38 @@
 /*global describe, beforeEach, it*/
 'use strict';
 
-var path    = require('path');
+var path	= require('path');
 var helpers = require('yeoman-generator').test;
 
 
 describe('can generator', function () {
-    beforeEach(function (done) {
-        helpers.testDirectory(path.join(__dirname, 'temp'), function (err) {
-            if (err) {
-                return done(err);
-            }
+	beforeEach(function (done) {
+		helpers.testDirectory(path.join(__dirname, 'temp'), function (err) {
+			if (err) {
+				return done(err);
+			}
 
-            this.app = helpers.createGenerator('can:app', [
-                '../../app'
-            ]);
-            done();
-        }.bind(this));
-    });
+			this.app = helpers.createGenerator('can:app', [
+				'../../app'
+			]);
+			return done();
+		}.bind(this));
+	});
 
-    it('creates expected files', function (done) {
-        var expected = [
-            // add files you expect to exist here.
-            '.jshintrc',
-            '.editorconfig'
-        ];
+	it('creates expected files', function (done) {
+		var expected = [
+			// add files you expect to exist here.
+			'.jshintrc',
+			'.editorconfig'
+		];
 
-        helpers.mockPrompt(this.app, {
-            'someOption': true
-        });
-        this.app.options['skip-install'] = true;
-        this.app.run({}, function () {
-            helpers.assertFiles(expected);
-            done();
-        });
-    });
+		helpers.mockPrompt(this.app, {
+			'someOption': true
+		});
+		this.app.options['skip-install'] = true;
+		this.app.run({}, function () {
+			helpers.assertFiles(expected);
+			done();
+		});
+	});
 });
