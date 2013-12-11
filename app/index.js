83008bb07db40c5b55ffe0113fb1658de92e509a
Josh Marchán <sykopomp@sykosomatic.org>
2013-12-11 14:06:24 -0800

    Tabify generator code

--- app/index.js
+++ app/index.js
@@ -5,68 +5,68 @@ var yeoman = require('yeoman-generator');
 var fs = require("fs");
 
 var CanGenerator = module.exports = function CanGenerator(args, options, config) {
-  yeoman.generators.Base.apply(this, arguments);
+	yeoman.generators.Base.apply(this, arguments);
 
-  this.on('end', function () {
-    this.installDependencies({ skipInstall: options['skip-install'] });
-  });
+	this.on('end', function () {
+		this.installDependencies({ skipInstall: options['skip-install'] });
+	});
 
-  this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
+	this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
 };
 
 util.inherits(CanGenerator, yeoman.generators.Base);
 
 CanGenerator.prototype.getAppName = function getAppName() {
-  var cb = this.async();
+	var cb = this.async();
 
-  this.prompt([{
-	  name: "appName",
-	  type: "input",
-	  message: "What should the app be called?",
-	  filter: function(x) { return x.trim(); },
-	  validate: function(x) { return /[a-z]+/i.test(x); }
-  }], function(props) {
-	this.opts = props;
-    cb();
-  }.bind(this));
+	this.prompt([{
+		name: "appName",
+		type: "input",
+		message: "What should the app be called?",
+		filter: function(x) { return x.trim(); },
+		validate: function(x) { return /[a-z]+/i.test(x); }
+	}], function(props) {
+		this.opts = props;
+		cb();
+	}.bind(this));
 };
 
 CanGenerator.prototype.basicFiles = function basicFiles() {
-  this.copy("gitignore", ".gitignore");
-  this.template('_package.json', 'package.json');
-  this.template('_bower.json', 'bower.json');
-  this.copy("editorconfig", ".editorconfig");
-  this.copy("jshintrc", ".jshintrc");
+	this.copy("gitignore", ".gitignore");
+	this.template('_package.json', 'package.json');
+	this.template('_bower.json', 'bower.json');
+	this.copy("editorconfig", ".editorconfig");
+	this.copy("jshintrc", ".jshintrc");
 };
 
 CanGenerator.prototype.gruntFiles = function gruntFiles() {
-  this.template("Gruntfile.js", "Gruntfile.js", {
-    mainPage: "<%= mainPage %>",
-    buildDir: "<%= buildDir %>",
-    version: "<%= version %>"
-  });
-  this.directory("tasks");
+	this.template("Gruntfile.js", "Gruntfile.js", {
+		mainPage: "<%= mainPage %>",
+		buildDir: "<%= buildDir %>",
+		version: "<%= version %>"
+	});
+	this.directory("tasks");
 };
 
 CanGenerator.prototype.docFiles = function docFiles() {
-  this.template("README.md", "README.md");
-  this.directory("doc", "doc");
+	this.template("README.md", "README.md");
+	this.directory("doc", "doc");
 };
 
 CanGenerator.prototype.stealFiles = function stealFiles() {
-  this.template("stealconfig.js", "stealconfig.js");
-  fs.symlinkSync(this.destinationRoot()+"/bower_components/steal",
-                 this.destinationRoot()+"/steal");
+	this.template("stealconfig.js", "stealconfig.js");
+	fs.symlinkSync(this.destinationRoot()+"/bower_components/steal",
+                   this.destinationRoot()+"/steal");
 };
 
 CanGenerator.prototype.testFiles = function testFiles() {
-  this.directory("test", "test");
-  // TODO - look into getting rid of these. Or at least organizing them
-  // better. Maybe put them inside test/?
-  this.directory("funcunit", "funcunit");
-  this.directory("qunit", "qunit");
+	this.directory("test", "test");
+	// TODO - look into getting rid of these. Or at least organizing them
+	// better. Maybe put them inside test/?
+	this.directory("funcunit", "funcunit");
+	this.directory("qunit", "qunit");
 };
 
 CanGenerator.prototype.srcDir = function srcDir() {
-  this.directory("src", "src");
+	this.directory("src", "src");
 };
--- component/index.js
+++ component/index.js
@@ -3,25 +3,25 @@ var util = require('util');
 var yeoman = require('yeoman-generator');
 
 var ComponentGenerator = module.exports = function ComponentGenerator(args, options, config) {
-  // By calling `NamedBase` here, we get the argument to the subgenerator call
-  // as `this.name`.
-  yeoman.generators.NamedBase.apply(this, arguments);
-  this.dashedName = this.name.replace("_", "-");
-  this.underscoredName = this.name.replace("-", "_");
-  if (!~this.dashedName.indexOf("-")) {
-    throw new Error("Component name must include at least one dash or underscore.");
-  }
-  this.destinationRoot("src/components/"+this.underscoredName);
-  this.mkdir(this.destinationRoot());
+	// By calling `NamedBase` here, we get the argument to the subgenerator call
+	// as `this.name`.
+	yeoman.generators.NamedBase.apply(this, arguments);
+	this.dashedName = this.name.replace("_", "-");
+	this.underscoredName = this.name.replace("-", "_");
+	if (!~this.dashedName.indexOf("-")) {
+		throw new Error("Component name must include at least one dash or underscore.");
+	}
+	this.destinationRoot("src/components/"+this.underscoredName);
+	this.mkdir(this.destinationRoot());
 };
 
 util.inherits(ComponentGenerator, yeoman.generators.NamedBase);
 
 ComponentGenerator.prototype.files = function files() {
-  this.template("_component.js", this.underscoredName+".js");
-  this.template("test.js", "test.js");
-  this.template("test.mustache", "test.mustache");
-  this.template("styles.less", "styles.less");
-  this.template("template.mustache", "template.mustache");
-  // TODO - sandbox.html?
+	this.template("_component.js", this.underscoredName+".js");
+	this.template("test.js", "test.js");
+	this.template("test.mustache", "test.mustache");
+	this.template("styles.less", "styles.less");
+	this.template("template.mustache", "template.mustache");
+	// TODO - sandbox.html?
 };
