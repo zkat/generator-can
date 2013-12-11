bbd26591ddc6811f75e31422f81b44e46ead28e9
Kat Marchán <kzm@sykosomatic.org>
2013-12-11 14:17:20 -0800

    Moved funcunit and qunit folders out of root

--- app/templates/stealconfig.js
+++ app/templates/stealconfig.js
@@ -3,10 +3,13 @@ steal.config({
 		"*": {
 			"jquery/jquery.js" : "jquery",
 			"can/util/util.js": "can/util/jquery/jquery.js",
-			"funcunit/qunit" : "qunit"
+			"funcunit/funcunit.js": "test/funcunit/funcunit.js",
+			"qunit/qunit.js": "test/funcunit/qunit/qunit.js"
 		}
 	},
 	paths: {
+		"qunit": "test/funcunit/qunit/qunit.js",
+		"funcunit": "test/funcunit/funcunit.js",
 		"can/": "bower_components/canjs/",
 		"jquery": "bower_components/canjs/lib/jquery.1.9.1.js"
 	},
